5dfcb1f3b6c482a8dff2098bc262ce997ce42243
Arthur Baker <artbaker82@gmail.com>
2022-02-19 15:53:48 -0800

    error handling

--- lib/tic_tak_toe.rb
+++ lib/tic_tak_toe.rb
@@ -3,10 +3,21 @@
 # module Game
   module Utils
 
-    def check_input(input)
-      valid_input = /\d\s\d/
-      valid_input.match(input)
+    def coordinates_are_valid?(x, y)
+      # valid_input = /[1-3]\s[1-3]/
+      # valid_input.match(input) ? true : false
+      x = x.to_i
+      y = y.to_i
+      x.between?(1,3) and y.between?(1,3) ? true : false
     end
+
+    def spot_available?(x, y)
+      #return boolean
+      x = x.to_i
+      y = y.to_i
+      self.grid[x - 1][y - 1] ? false : true
+    end
+
 
   end
 
@@ -21,21 +32,44 @@
       @current_player
     end
 
-    
     def add_player(player)
       players.push(player)
     end
 
     def get_input_and_update_board 
-      print "#{self.current_player[:name]}, where do you choose to place your #{self.current_player[:char]}? "
-      input = gets
-      if check_input(input)
-        self.update_board(input, self.current_player[:char])
-      else
-        puts 'Please enter two digits separated by a space'
-        self.get_input
-      end
+      puts "#{self.current_player[:name]}, choose the x coordinate to place your piece. "      
+      x = gets
+      puts "#{self.current_player[:name]}, choose the y coordinate to place your piece. "
+      y = gets
+      input = "#{x}#{y}"
 
+      return print_error("Please enter coordiantes between 1 and 3") unless coordinates_are_valid?(x, y)
+      return print_error("The coordinates #{x} #{y} are already taken, please choose other coordinates.") unless spot_available?(x, y)
+
+      self.update_board(x, y, self.current_player[:char])
+    end
+
+    def check_for_winner
+      #algorithm to check board changes game_over state to true or false
+      unless self.game_over == true
+        #switch player
+        #get index of current player
+        idx_of_current_player = self.players.index(self.current_player)
+        #ternary operator to switch players
+        self.current_player = idx_of_current_player == 0 ? self.players[1] : self.players[0]
+      else
+        #game is over
+        #move this to separate function?
+        puts "Congrats #{self.current_player}, you are the winner."
+        puts "Would you like to play another game? [Y/n]"
+        play_again = gets
+
+      end
+    end
+
+    def print_error(error_message)
+      puts error_message
+      self.get_input_and_update_board
     end
 
 
@@ -54,13 +88,10 @@
       print self.grid
     end
 
-    def update_board(coordinates, char)
-      #coordinates are sanitizes to be two digits, separated by a space
-      x = coordinates.split[0].to_i - 1
-      y = coordinates.split[1].to_i - 1
-      print "y #{y}"
-      print "x #{x}"
-      self.grid[x][y] = char
+    def update_board(x, y, char)
+      x = x.to_i
+      y = y.to_i
+      self.grid[x - 1][y - 1] = char
     end
   end
 
@@ -86,7 +117,8 @@ game.current_player = game.players[0]
 until game.game_over == true do
   game.get_input_and_update_board
   game.print_board
-  # game.check_for_winner
+  game.check_for_winner
+
 end
 
 
@@ -94,7 +126,6 @@ end
 
 
 #TODO 
-# right now, inputs can be any number, in the check input method, make sure the input is
-# two digits between 1 and 3.
-# in the update_board method, make sure that subtracting by 1 will work.
-# get board to print to terminal in correct format
+#switch players
+#get board to print to terminal in correct format
+
